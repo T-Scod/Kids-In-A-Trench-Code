70ce62c632eb02aa6ef13f49dd784486a940128c
Lachlan Mesman <lmesman2000@gmail.com>
2019-04-11 13:57:55 +1000

    Merge remote-tracking branch 'origin/master'

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,55 @@ public class Player : MonoBehaviour
 {
     private Rigidbody rigidbody;
     private Vector3 velocity;
+    public float m_speed;
+    private float m_speedModifier;
 
-	private void Start ()
+    private float dashTime;
+
+
+    private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-	}
-	
-	void Update ()
+    }
+
+    void Update()
     {
-        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical")).normalized * 10.0f;
-	}
+        if (GetComponent<PlayerInput>().sprint == true)
+        {
+            m_speedModifier = 2f;
+        }
+        else
+        {
+            m_speedModifier = 1f;
+        }
+
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical")).normalized * m_speed * m_speedModifier;
+
+        if (GetComponent<PlayerInput>().shoot == true)
+            Shoot();
+        if (GetComponent<PlayerInput>().dash == true && dashTime == 0)
+            Dash();
+    }
 
     private void FixedUpdate()
     {
         rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    void Dash()
+    {
+        float dashTime = 5;
+        if (dashTime > 0)
+        {
+            m_speedModifier = 40f;
+            dashTime--;
+        }
+        if (dashTime == 0)
+            m_speedModifier = 1;
+    }
+
+    void Shoot()
+    {
+
     }
 }
