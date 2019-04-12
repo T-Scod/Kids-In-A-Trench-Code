569d862f5c0f0b63d44584844a345dfc8736afb2
Will <william.brazil.design@gmail.com>
2019-04-12 11:30:31 +1000

    Updates to Title Scene

---
+++ Assets/Scripts/Title/LightFlasher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LightFlasher : MonoBehaviour
+{
+
+    private Light thisLight;
+
+    private float baseValue;
+    public float amplitude = 5f;
+    public float frequency = 6f;
+
+
+    void Start()
+    {
+        // get reference to the light on this object
+        thisLight = GetComponent<Light>();
+        // set start intensity
+        baseValue = thisLight.intensity;
+    }
+
+
+
+    void Update()
+    {
+        float currentValue = amplitude * Mathf.Sin(frequency * Time.time);
+        thisLight.intensity = baseValue + currentValue;
+    }
+
+}
+
---
+++ Assets/Scripts/Title/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+
+}
